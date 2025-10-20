942a3693a4e6e667a02891abe50b01e65751890a
ihttr <155672422+ihttr@users.noreply.github.com>
2025-10-20 13:57:02 +0300

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-# 1. تثبيت مكتبات البايثون
-pip install -r requirements.txt
-
-# 2. تثبيت ffmpeg على الخادم
-apt-get update && apt-get install -y ffmpeg
